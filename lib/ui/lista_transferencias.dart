cb1bb8cdf36b1f804597a34963ac51645b12444d
Josue Lopes <josue10599@gmail.com>
2019-11-05 18:44:39 -0300

    Adicionada a função de atualizar a lista e corrigido o bug ao abrir o formulario e não adicionar uma transação

--- lib/ui/lista_transferencias.dart
+++ lib/ui/lista_transferencias.dart
@@ -4,13 +4,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TelaTransferencias extends StatelessWidget {
+  final List<Transferencia> _transferencias = List();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Transferências"),
       ),
-      body: ListaDeTransferencias(),
+      body: ListaDeTransacoes(_transferencias),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           final Future<Transferencia> transaferencia =
@@ -18,7 +20,9 @@ class TelaTransferencias extends StatelessWidget {
             return TelaFormularioTransferencia();
           }));
           transaferencia.then((transaferenciaRecebida) {
-            debugPrint(transaferenciaRecebida.toString());
+            if (transaferenciaRecebida != null) {
+              _transferencias.add(transaferenciaRecebida);
+            }
           });
         },
         child: Icon(Icons.add),
@@ -27,32 +31,47 @@ class TelaTransferencias extends StatelessWidget {
   }
 }
 
-class ListaDeTransferencias extends StatelessWidget {
+class ListaDeTransacoes extends StatefulWidget {
+  final List<Transferencia> _transferencias;
+
+  ListaDeTransacoes(this._transferencias);
+
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(100.0, 2034)),
-        ItemTransferencia(Transferencia(120.0, 2554)),
-      ],
-    );
-  }
+  _ListaDeTransacoesState createState() => _ListaDeTransacoesState();
 }
 
-class ItemTransferencia extends StatelessWidget {
-  const ItemTransferencia(this._transferencia);
-
-  final Transferencia _transferencia;
-
+class _ListaDeTransacoesState extends State<ListaDeTransacoes> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: Icon(Icons.monetization_on),
-        title: Text("R\$ ${_transferencia.valor.toStringAsFixed(2)}"),
-        subtitle: Text("Número da Conta: ${_transferencia.numeroConta}"),
-        onTap: () {},
-      ),
+    return ListView.builder(
+      padding: EdgeInsets.all(8.0),
+      itemCount: widget._transferencias.length,
+      itemBuilder: (context, index) => ItemTransferencia(widget._transferencias[index]),
     );
   }
+
+  Widget ItemTransferencia(Transferencia transferencia) =>
+      Card(
+        child: ListTile(
+          leading: Icon(
+            Icons.monetization_on,
+            color: Colors.green,
+            size: 32.0,
+          ),
+          title: Text(
+            "R\$ ${transferencia.valor.toStringAsFixed(2)}",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 24.0,
+            ),
+          ),
+          subtitle: Text(
+            "Número da Conta: ${transferencia.numeroConta}",
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          onTap: () {},
+        ),
+      );
 }
